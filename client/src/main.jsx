2ea72e00a56d0622a6c8a4538b17db503eb993cd
brentrwill <brentrwill@gmail.com>
2026-07-02 16:49:32 -0600

    Deploy Vite app to GitHub Page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles.css";
 
-// This starts the React app and places it inside the div with id="root"
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Baseball-Tracker">
       <App />
     </BrowserRouter>
   </React.StrictMode>
