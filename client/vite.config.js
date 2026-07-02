8b651f382720f4bb4cf88017e90f20c53fb5472d
brentrwill <brentrwill@gmail.com>
2026-07-02 16:29:21 -0600

    Deploy Vite app to GitHub Page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Baseball-Tracker/",
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:3001'
+      "/api": "http://localhost:3001"
     }
   }
 });
